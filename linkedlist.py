9b4f451b5097a9cef538217e3c27bb044273d7d2
firdavsxon <akilovfs@gmail.com>
2020-06-10 13:25:14 -0700

    linked list

---
+++ linkedlist.py
@@ -0,0 +1,94 @@
+class Node:
+
+    def __init__(self, value):
+        self.info = value
+        self.link = None
+
+
+class SingleLinkedList:
+
+    def __init__(self):
+        self.start = None
+
+    def display_list(self):
+        pass
+
+    def count_node(self):
+        pass
+
+    def search(self):
+        pass
+
+    def insert_in_beginning(self, data):
+        pass
+
+    def insert_at_end(self, data):
+        pass
+
+    def create_list(self):
+        pass
+
+    def insert_after(self, data, x):
+        pass
+
+    def insert_before(self):
+        pass
+
+    def insert_at_position(self, data, k):
+        pass
+
+    def delete_node(self, x):
+        pass
+
+    def delete_fisrt_node(self):
+        pass
+
+    def delete_last_node(self):
+        pass
+
+    def reverse_list(self):
+        pass
+
+    def bubble_sort_exdata(self):
+        pass
+
+    def buble_sort_exlink(self):
+        pass
+
+    def has_cycle(self):
+        pass
+
+    def find_cycle(self):
+        pass
+
+    def remove_cycle(self):
+        pass
+
+    def insert_cycle(self, x):
+        pass
+
+    def merge2(self, list2):
+        pass
+
+    def _merge2(self, p1, p2):
+        pass
+
+    def merge_sort(self):
+        pass
+
+    def _merge_sort_rec(self, listStart):
+        pass
+
+    def _divide_list(self, p):
+        pass
+
+
+
+
+
+
+
+
+
+
+
---
+++ practice.py
@@ -0,0 +1,28 @@
+from linkedlist import Node, SingleLinkedList
+
+
+obj_list = SingleLinkedList()
+obj_list.create_list()
+
+while True:
+	print("1. Display list")
+	print("2. Count the number of nodes")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+
+
